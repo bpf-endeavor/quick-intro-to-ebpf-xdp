be8e247713f424d9178f1e28f8f34819e163e0c7
Farbod Shahinfar <fshahinfar1@gmail.com>
2025-04-13 15:28:33 +0200

    update scripts

---
+++ README.md
@@ -0,0 +1,8 @@
+# A Quick Introduction to eBPF/XDP
+
+## How it is organized?
+
+## Some questions
+
+**Why No Makefile?**
+* The goal is for the reader to see all the necessary steps, tools, commands, ...
---
+++ scripts/install_reqs.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -x
+
+curdir=$(dirname $0)
+mkdir _tmp/
+cd _tmp/
+
+# install llvm/clang
+wget https://apt.llvm.org/llvm.sh
+chmod +x llvm.sh
+sudo ./llvm.sh 15
+sudo bash "$curdir"/update-alternatives-clang.sh 15 100
+
+# install libbpf
+cd "$curdir/_tmp/"
+mkdir libbpf
+cd ./libbpf
+_VERSION="1.4.7"
+VERSION="v$_VERSION"
+wget https://github.com/libbpf/libbpf/archive/refs/tags/$VERSION.tar.gz
+tar -xf $VERSION.tar.gz
+cd libbpf-$_VERSION/src/
+make
+sudo make install
+echo "/usr/lib64/" | sudo tee /etc/ld.so.conf.d/libbpf.conf
+echo 'export PKG_CONFIG_PATH="$PKG_CONFIG_PATH:/lib64/pkgconfig/"' | tee -a $HOME/.bashrc
+sudo ldconfig
+
+# install bpftool
+sudo apt update
+sudo apt install -y bpftool
